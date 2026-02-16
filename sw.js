628878bd2b1272ed4ad10f7ea9395e100173167a
vaiswanara <71589931+vaiswanara@users.noreply.github.com>
2026-02-16 05:57:04 +0530

    Update cache name to family-tree-cache-v3.7.0

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * =====================================================================================
  */
 
-const CACHE_NAME = 'family-tree-cache-v3.6.8';
+const CACHE_NAME = 'family-tree-cache-v3.7.0';
 
 // All the files and assets the app needs to function offline.
 const URLS_TO_CACHE = [
