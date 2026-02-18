295d640546f7bdaf5d8050c03da591b6f81cedac
vaiswanara <71589931+vaiswanara@users.noreply.github.com>
2026-02-18 07:00:17 +0530

    V5.0.18

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * =====================================================================================
  */
 
-const CACHE_NAME = 'family-tree-cache-v5.0.16';
+const CACHE_NAME = 'family-tree-cache-v5.0.18';
 
 // All the files and assets the app needs to function offline.
 const URLS_TO_CACHE = [
